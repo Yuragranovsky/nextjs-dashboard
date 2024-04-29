3e99c69dc92cb5465c0d037eea291f0c616381c2
Yuragranovsky <yuragranovsky@gmail.com>
2024-04-29 12:49:54 +0300

    fix authConfig path

--- app/pages/api/[...nextauth].ts
+++ app/pages/api/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
+import { authConfig } from '../../../auth.config';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
